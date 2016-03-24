58ad1da1044ee5574a59eac04f060b813d518137
User <user@Users-MacBook-Pro.local>
2016-03-23 20:09:45 -0500

    Updated console output

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 // @author: Kyle Johnson
 // date: 3/23/2016
+
+  console.log("                                 _                                | \\n                                | |\n                                | |\n           |\                   | |\n          /, ~\                / /\n         X     `-.....-------./ /\n          ~-. ~  ~              |\n             \             /    |\n              \  /_     ___\   /\n              | /\ ~~~~~   \ |\n              | | \        || |\n              | |\ \       || )\n             (_/ (_/      ((_/");
+
+
+// TODO: learn line breaks with ASCII art
